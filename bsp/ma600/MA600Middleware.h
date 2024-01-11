9c6a136902e0882d2dab9c8d8fc66268d4f03251
Hutao <2522959889@qq.com>
2024-01-11 18:30:35 +0800

    补丁1

--- bsp/ma600/MA600Middleware.h
+++ bsp/ma600/MA600Middleware.h
@@ -1,11 +1,17 @@
 #ifndef MA600_MIDDLEWARE_H
 #define MA600_MIDDLEWARE_H
 
+#include "rfl_config.h"
+
+#if (RFL_CONFIG_CORE == RFL_CORE_WPIE_HPM6750)
 #include "board.h"
 #include "hpm_gpio_drv.h"
 #include "hpm_gpiom_drv.h"
 #include "hpm_spi_drv.h"
-#include "rfl_config.h"
+
+#elif (RFL_CONFIG_CORE == RFL_CORE_RM_C_BORAD)
+
+#endif
 
 #define NUM_OF_MA600 1 // 使用的MA600个数
 

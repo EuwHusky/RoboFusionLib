9e877cb62b457286879e218012106d1fdf7c132e
EuwHusky <ehuskyoc@outlook.com>
2024-07-18 12:32:27 +0800

    芯片驱动层延时模块：删除硬件延时功能，仅保留freertos操作系统延时

--- driver/delay/drv_delay.c
+++ driver/delay/drv_delay.c
@@ -1,93 +1,9 @@
 #include "drv_delay.h"
 
-#if RFL_CONFIG_CORE == RFL_CORE_WPIE_HPM6750
 #include "FreeRTOS.h"
 #include "task.h"
 
-#elif RFL_CONFIG_CORE == RFL_CORE_RM_C_BORAD
-#include "main.h"
-
-#include "cmsis_os.h"
-
-static uint8_t fac_us = 0;
-static uint32_t fac_ms = 0;
-
-void delay_init(void)
+void rflOsDelayMs(uint32_t ms)
 {
-    fac_us = SystemCoreClock / 1000000;
-    fac_ms = SystemCoreClock / 1000;
-}
-
-void delay_us(uint16_t nus)
-{
-    uint32_t ticks = 0;
-    uint32_t told = 0;
-    uint32_t tnow = 0;
-    uint32_t tcnt = 0;
-    uint32_t reload = 0;
-    reload = SysTick->LOAD;
-    ticks = nus * fac_us;
-    told = SysTick->VAL;
-    while (1)
-    {
-        tnow = SysTick->VAL;
-        if (tnow != told)
-        {
-            if (tnow < told)
-            {
-                tcnt += told - tnow;
-            }
-            else
-            {
-                tcnt += reload - tnow + told;
-            }
-            told = tnow;
-            if (tcnt >= ticks)
-            {
-                break;
-            }
-        }
-    }
-}
-
-void delay_ms(uint16_t nms)
-{
-    uint32_t ticks = 0;
-    uint32_t told = 0;
-    uint32_t tnow = 0;
-    uint32_t tcnt = 0;
-    uint32_t reload = 0;
-    reload = SysTick->LOAD;
-    ticks = nms * fac_ms;
-    told = SysTick->VAL;
-    while (1)
-    {
-        tnow = SysTick->VAL;
-        if (tnow != told)
-        {
-            if (tnow < told)
-            {
-                tcnt += told - tnow;
-            }
-            else
-            {
-                tcnt += reload - tnow + told;
-            }
-            told = tnow;
-            if (tcnt >= ticks)
-            {
-                break;
-            }
-        }
-    }
-}
-#endif
-
-void rflOsDelayMs(uint16_t ms)
-{
-#if RFL_CONFIG_CORE == RFL_CORE_WPIE_HPM6750
     vTaskDelay(ms);
-#elif RFL_CONFIG_CORE == RFL_CORE_RM_C_BORAD
-    osDelay(ms);
-#endif
 }
--- driver/delay/drv_delay.h
+++ driver/delay/drv_delay.h
@@ -3,15 +3,6 @@
 
 #include "stdint.h"
 
-#include "rfl_config.h"
-
-#if RFL_CONFIG_CORE == RFL_CORE_WPIE_HPM6750
-#elif RFL_CONFIG_CORE == RFL_CORE_RM_C_BORAD
-extern void delay_init(void);
-extern void delay_us(uint16_t nus);
-extern void delay_ms(uint16_t nms);
-#endif
-
-extern void rflOsDelayMs(uint16_t ms);
+extern void rflOsDelayMs(uint32_t ms);
 
 #endif /* _DRV_DELAY_H__ */
